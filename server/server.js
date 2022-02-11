103edf2bee55b0efb2047775ee4eae95fd4a0438
JohnRamberger <jramberger3@gatech.edu>
2022-02-11 02:00:43 -0500

    created api commands

--- server/server.js
+++ server/server.js
@@ -28,101 +28,140 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//---------------------------------
+
+function checkAPIkey(key, requiredScope) {
+  let hash1 = md5(key);
+  //verify key
+  let sql = `SELECT scopes FROM api_keys WHERE key = '${hash1}'`;
+  db.all(sql, (err, rows) => {
+    if (err) {
+      //res.status(400).json({ error: err.message });
+      return err.message;
+    }
+    if (rows.length <= 0) {
+      //res.status(400).json({ error: "API Key does not exist" });
+      return "API Key does not exist";
+    }
+    if (rows.length > 1) {
+      //res.status(400).json({ error: "duplicate API Keys exist" });
+      return "duplicate API Keys exist";
+    }
+    //verify scope
+    if (!hasScope(rows, requiredScope)) {
+      //res.status(400).json({ error: "Key scope does not allow this request" });
+      return "Key scope does not allow this request";
+    }
+    return;
+  });
+}
+
+function hasScope(rows, required) {
+  let row = rows[0];
+  let scopes = new Array(row.scopes.split(","));
+
+  if (required == "*") {
+    //master
+    if (
+      (scopes.length > 1 && scopes.includes("*")) ||
+      (scopes.length == 1 && scopes == "*")
+    ) {
+      return true;
+    }
+  } else if (required.includes(".")) {
+    //required.*
+    if (
+      (scopes.length > 1 && scopes.includes(required.split(".")[0] + ".*")) ||
+      (scopes.length == 1 && scopes == required.split(".")[0] + ".*")
+    ) {
+      return true;
+    }
+    //required.required
+    if (
+      (scopes.length > 1 && scopes.includes(required)) ||
+      (scopes.length == 1 && scopes == required)
+    ) {
+      return true;
+    }
+  }
+
+  //incorrect scope or error
+  return false;
+}
+
 //-----------------requests---------------
 
-app.get("/api/apikeys", (req, res) => {
+app.get("/api/keys/:key/list", (req, res) => {
+  let key = req.params.key;
+  let err = checkAPIkey(key, "keys.list");
+  if (err) {
+    res.status(400).json({ error: err });
+    return;
+  }
+
+  //get all api keys
   var sql = "select * from api_keys";
   var params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
-      res.status(400).json(JSON.stringify({ error: err.message }));
+      res.status(400).json({ error: err.message });
       return;
     }
-    res.json(
-      JSON.stringify({
-        message: "success",
-        data: rows,
-      })
-    );
+    res.json({
+      message: "success",
+      data: rows,
+    });
   });
 });
 
-app.get("/api/apikeys/:key/generatemaster", (req, res) => {
+app.get("/api/keys/:key/generatemaster", (req, res) => {
   let key = req.params.key;
-  let hash1 = md5(key);
-  //verify key
-  let sql = `SELECT scopes FROM api_keys WHERE key = '${hash}'`;
-  db.all(sql, (err, rows) => {
+  let err = checkAPIkey(key, "*");
+  if (err) {
+    res.status(400).json({ error: err });
+    return;
+  }
+
+  //generate new key
+  let prefix = generateApiKey({ method: "bytes", length: 7 });
+  let affix = generateApiKey({ method: "bytes", length: 20 });
+  let apikey = prefix + "." + affix;
+  let hash2 = md5(apikey);
+  var sql = `INSERT INTO api_keys (name, prefix, key, scopes) VALUES ('Master', '${prefix}', '${hash2}', '*');`;
+  db.run(sql, (err) => {
     if (err) {
-      res.status(400).json(JSON.stringify({ error: err.message }));
+      res.status(400).json({ error: err.message });
       return;
     }
-    if (rows.length <= 0) {
-      res.status(400).json(JSON.stringify({ error: "API Key does not exist" }));
-      return;
-    }
-    if (rows.legnth > 1) {
-      res
-        .status(400)
-        .json(JSON.stringify({ error: "duplicate API Keys exist" }));
-      return;
-    }
-    if (!rows.scopes.split(",").include("*")) {
-      res
-        .status(400)
-        .json(
-          JSON.stringify({ error: "Key scope does not allow this request" })
-        );
-      return;
-    } else {
-      //generate new key
-      let prefix = generateApiKey({ method: "bytes", length: 7 });
-      let affix = generateApiKey({ method: "bytes", length: 20 });
-      let apikey = prefix + "." + affix;
-      let hash2 = md5(apikey);
-      var sql = `INSERT INTO api_keys (name, prefix, key, scopes) VALUES ('Master', '${prefix}', '${hash2}', '*');`;
-      db.run(sql, (err) => {
-        if (err) {
-          res.status(400).json(JSON.stringify({ error: err.message }));
-          return;
-        }
-        res.json(
-          JSON.stringify({
-            message: "success",
-            apikey: apikey,
-          })
-        );
-      });
-    }
+    res.json({
+      message: "success",
+      apikey: apikey,
+    });
   });
 });
 
-app.get("/api/apikeys/:key/test", (req, res) => {
+app.get("/api/keys/:key/test", (req, res) => {
   let key = req.params.key;
   let hash = md5(key);
   //verify key
   let sql = `SELECT scopes FROM api_keys WHERE key = '${hash}'`;
   db.all(sql, (err, rows) => {
     if (err) {
-      res.status(400).json(JSON.stringify({ error: err.message }));
+      res.status(400).json({ error: err.message });
       return;
     }
     if (rows.length <= 0) {
-      res.status(400).json(JSON.stringify({ error: "API Key does not exist" }));
+      res.status(400).json({ error: "API Key does not exist" });
       return;
     }
-    if (rows.legnth > 1) {
-      res
-        .status(400)
-        .json(JSON.stringify({ error: "duplicate API Keys exist" }));
+    if (rows.length > 1) {
+      res.status(400).json({ error: "duplicate API Keys exist" });
       return;
     }
-    res.json(
-      JSON.stringify({
-        message: "success",
-        data: rows,
-      })
-    );
+    res.json({
+      message: "success",
+      data: rows,
+    });
   });
 });
 
