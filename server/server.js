1bbb65a4579b591c25e988f7b20fbabb6698b1cf
JohnRamberger <jramberger3@gatech.edu>
2022-02-11 01:28:14 -0500

    setup api system

--- server/server.js
+++ server/server.js
@@ -1,64 +1,132 @@
-const express = require('express')
-const cors = require('cors')
-
-const { graphqlHTTP } = require('express-graphql')
-const gql = require('graphql-tag')
-const { buildASTSchema } = require('graphql')
-const { readFileSync } = require('fs')
-
+//load express stuff
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
 const port = 5069;
 
-const app = express()
-app.use(cors())
+//load other things
+const generateApiKey = require("generate-api-key");
 
-const query = readFileSync('./graphql/schema.graphql').toString('utf-8');
+//load the database
+const db = require("./db/database.js");
+var md5 = require("md5");
 
+// Where we will keep books
+let books = {
+  isbn: "9781593275846",
+  title: "Eloquent JavaScript, Second Edition",
+  author: "Marijn Haverbeke",
+  publish_date: "2014-12-14",
+  publisher: "No Starch Press",
+  numOfPages: "472",
+};
 
-const schema = buildASTSchema(gql(query))
+app.use(cors());
 
-const PEOPLE = new Map()
-const POSTS = new Map()
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-class Post {
-  constructor (data) { Object.assign(this, data) }
-  get author () {
-    return PEOPLE.get(this.authorId)
-  }
-}
+//-----------------requests---------------
 
-class Person {
-  constructor (data) { Object.assign(this, data) }
-  get posts () {
-    return [...POSTS.values()].filter(post => post.authorId === this.id)
-  }
-}
+app.get("/api/apikeys", (req, res) => {
+  var sql = "select * from api_keys";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json(JSON.stringify({ error: err.message }));
+      return;
+    }
+    res.json(
+      JSON.stringify({
+        message: "success",
+        data: rows,
+      })
+    );
+  });
+});
 
-const rootValue = {
-  posts: () => POSTS.values(),
-  post: ({ id }) => POSTS.get(id),
-  authors: () => PEOPLE.values(),
-  author: ({ id }) => PEOPLE.get(id)
-}
+app.get("/api/apikeys/:key/generatemaster", (req, res) => {
+  let key = req.params.key;
+  let hash1 = md5(key);
+  //verify key
+  let sql = `SELECT scopes FROM api_keys WHERE key = '${hash}'`;
+  db.all(sql, (err, rows) => {
+    if (err) {
+      res.status(400).json(JSON.stringify({ error: err.message }));
+      return;
+    }
+    if (rows.length <= 0) {
+      res.status(400).json(JSON.stringify({ error: "API Key does not exist" }));
+      return;
+    }
+    if (rows.legnth > 1) {
+      res
+        .status(400)
+        .json(JSON.stringify({ error: "duplicate API Keys exist" }));
+      return;
+    }
+    if (!rows.scopes.split(",").include("*")) {
+      res
+        .status(400)
+        .json(
+          JSON.stringify({ error: "Key scope does not allow this request" })
+        );
+      return;
+    } else {
+      //generate new key
+      let prefix = generateApiKey({ method: "bytes", length: 7 });
+      let affix = generateApiKey({ method: "bytes", length: 20 });
+      let apikey = prefix + "." + affix;
+      let hash2 = md5(apikey);
+      var sql = `INSERT INTO api_keys (name, prefix, key, scopes) VALUES ('Master', '${prefix}', '${hash2}', '*');`;
+      db.run(sql, (err) => {
+        if (err) {
+          res.status(400).json(JSON.stringify({ error: err.message }));
+          return;
+        }
+        res.json(
+          JSON.stringify({
+            message: "success",
+            apikey: apikey,
+          })
+        );
+      });
+    }
+  });
+});
 
-const initializeData = () => {
-  const fakePeople = [
-    { id: '1', firstName: 'John', lastName: 'Doe' },
-    { id: '2', firstName: 'Jane', lastName: 'Doe' }
-  ]
+app.get("/api/apikeys/:key/test", (req, res) => {
+  let key = req.params.key;
+  let hash = md5(key);
+  //verify key
+  let sql = `SELECT scopes FROM api_keys WHERE key = '${hash}'`;
+  db.all(sql, (err, rows) => {
+    if (err) {
+      res.status(400).json(JSON.stringify({ error: err.message }));
+      return;
+    }
+    if (rows.length <= 0) {
+      res.status(400).json(JSON.stringify({ error: "API Key does not exist" }));
+      return;
+    }
+    if (rows.legnth > 1) {
+      res
+        .status(400)
+        .json(JSON.stringify({ error: "duplicate API Keys exist" }));
+      return;
+    }
+    res.json(
+      JSON.stringify({
+        message: "success",
+        data: rows,
+      })
+    );
+  });
+});
 
-  fakePeople.forEach(person => PEOPLE.set(person.id, new Person(person)))
-
-  const fakePosts = [
-    { id: '1', authorId: '1', body: 'Hello world' },
-    { id: '2', authorId: '2', body: 'Hi, planet!' }
-  ]
-
-  fakePosts.forEach(post => POSTS.set(post.id, new Post(post)))
-}
-
-initializeData()
-
-app.use('/graphql', graphqlHTTP({ schema, rootValue }))
-
-app.listen(port)
-console.log(`Running a GraphQL API server at localhost:${port}/graphql`)
+//---------------end of requests----------
+app.listen(port, () =>
+  console.log(`Hello world app listening on port ${port}!`)
+);
